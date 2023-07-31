47bc0734cc938abfb455f4804f9c3dfa53e608b2
muhammadislom <islom.pr@gmail.com>
2023-07-31 15:10:18 +0500

    add app script

---
+++ docker/php-fpm/core/app.php
@@ -0,0 +1,37 @@
+<?php
+
+shell_exec('composer create laravel/laravel laravel');
+
+
+$env = explode(PHP_EOL, file_get_contents(__DIR__ . '/laravel/.env'));
+
+$_env = explode(PHP_EOL, file_get_contents('/app/.env'));
+
+$env_prod = '';
+foreach ($_env as $key => &$_environment) {
+    foreach ($env as $_key => &$environment) {
+        $_e = explode('=', $_environment);
+        $e = explode('=', $environment);
+        if ($_e[0] == $e[0]) {
+            $environment = $e[0] . '=' . $_e[1];
+            $_environment = null;
+            continue 2;
+        }
+
+    }
+}
+foreach ($_env as $key => &$_environment) {
+    if (isset($_environment)) {
+        $_e = explode('=', $_environment);
+        $env[] = $_e[0] . '=' . $_e[1];
+    }
+}
+
+foreach ($env as $_env) {
+    $env_prod .= $_env ."\n";
+}
+
+file_put_contents(__DIR__ . '/laravel/.env', $env_prod);
+
+shell_exec('mv  laravel/* laravel/.* ../../../');
+//echo "<pre>$output</pre>";
