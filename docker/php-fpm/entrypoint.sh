716d5ed7bb9e16cf87e0534315165416ee2a002f
muhammadislom <islom.pr@gmail.com>
2023-08-01 10:33:33 +0500

    edit shell script

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 pwd
-if [  -f "/app/public/index.php" ];
+if [ ! -f "/app/public/index.php" ];
 then
   php docker/php-fpm/core/app.php
   echo "Installed )"
 else
   echo "laravel is already installed"
 fi
+
+exec "$@";
